f99e1b3e631ccfdcbcd6a0223628bb97171628a1
Clément Polet <cpolet@ubikod.com>
2013-10-08 16:41:58 +0200

    ERMIN-5644 * Replace in-app notif by system notif * Add Capptain video 856ko

--- src/groovy/capptain/video/push/demo/frontend/constants/AnnouncementDefaultValues.groovy
+++ src/groovy/capptain/video/push/demo/frontend/constants/AnnouncementDefaultValues.groovy
@@ -4,8 +4,8 @@ class AnnouncementDefaultValues
 {
   final static KIND = "announcement"
   final static CATEGORY = "videopush"
-  final static DELIVERY_TIME = "session"
-  final static NOTIFICATION_TYPE = "popup"
+  final static DELIVERY_TIME = "any"
+  final static NOTIFICATION_TYPE = "system"
   final static ACTION_BUTTON_TEXT = "Click me!"
   final static EXIT_BUTTON_TEXT = "Exit"
 }
